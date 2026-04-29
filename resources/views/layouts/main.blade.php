ed663c572e8ad76d0012943300c73e184c8da610
Kirill <kirillnik522@gmail.com>
2026-04-29 22:42:12 +0500

    style: добавил иконку заказов на главном макете

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -20,10 +20,9 @@
                 </svg>
             </a>
             <a href="{{ route('orders.index') }}" class="relative inline-block text-gray-700 hover:text-blue-600">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24"
-                     stroke="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                          d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"/>
+                          d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" />
                 </svg>
             </a>
             @if (Auth::check())
